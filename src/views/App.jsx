3e93c59c13dac8cee39b6a51c9b28d1086578577
Diogozura <65250673+Diogozura@users.noreply.github.com>
2021-05-08 17:50:30 -0300

    sa

---
+++ src/views/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+
+import Menu from '../components/Menu'
+
+
+const App = props => (
+    <div className="App">
+        Component App
+        <Menu/>
+    </div>
+)
+
+export default App
